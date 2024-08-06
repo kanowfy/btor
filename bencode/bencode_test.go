2fb681e79510769efd15a97a862e0b71e9bc8b6f
kanowfy <nguyendung2002hl@gmail.com>
2024-08-06 22:00:46 +0700

    add bencode decode unit test

--- bencode/bencode_test.go
+++ bencode/bencode_test.go
@@ -8,6 +8,83 @@ import (
 	"github.com/kanowfy/btor/bencode"
 )
 
+func TestDecode_CorrectlyDecodesBencodeString(t *testing.T) {
+	t.Parallel()
+	cases := []struct {
+		name    string
+		bencode string
+		want    interface{}
+	}{
+		{
+			name:    "integer",
+			bencode: "i42e",
+			want:    42,
+		},
+		{
+			name:    "negative integer",
+			bencode: "i-42e",
+			want:    -42,
+		},
+		{
+			name:    "string",
+			bencode: "5:hello",
+			want:    "hello",
+		},
+		{
+			name:    "list",
+			bencode: "l5:helloi42ee",
+			want: []interface{}{
+				"hello",
+				42,
+			},
+		},
+		{
+			name:    "dictionary",
+			bencode: "d5:hello5:world3:foo3:bare",
+			want: map[string]interface{}{
+				"hello": "world",
+				"foo":   "bar",
+			},
+		},
+		{
+			name:    "nested dictionary",
+			bencode: "d3:food3:bar3:baz6:numberi42eee",
+			want: map[string]interface{}{
+				"foo": map[string]interface{}{
+					"bar":    "baz",
+					"number": 42,
+				},
+			},
+		},
+		{
+			name:    "list of dictionary",
+			bencode: "ld3:foo3:bared3:bar3:baz6:numberi42eee",
+			want: []interface{}{
+				map[string]interface{}{
+					"foo": "bar",
+				},
+				map[string]interface{}{
+					"bar":    "baz",
+					"number": 42,
+				},
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			got, _, err := bencode.Decode(tc.bencode, 0)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if !cmp.Equal(tc.want, got) {
+				t.Error(cmp.Diff(tc.want, got))
+			}
+		})
+	}
+}
+
 func TestEncode_CorrectlyEncodesMap(t *testing.T) {
 	t.Parallel()
 
